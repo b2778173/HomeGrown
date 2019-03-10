a8d952e7030fce4b346a25febac3074e64e080fd
Kevin Tsai <b2778173@hotmail.com>
2019-03-10 15:16:25 +0800

    Update header.vue

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark" style="background-color: #e3f2fd;">
       <a class="navbar-brand" href="#">Navbar</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav">
           <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
