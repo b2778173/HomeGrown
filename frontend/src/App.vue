9a63d02f35da02860284503ac24c047893c43e74
Kevin Tsai <b2778173@hotmail.com>
2019-03-10 15:16:49 +0800

    App Update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <div id="app">
-    <v-header></v-header>
+  <div id="app" style="width: 100%;">
     <router-view/>
   </div>
 </template>
 
 <script>
-import vHeader from '@/components/header.vue'
 
 export default {
   name: 'App',
   components: {
-    vHeader
+    
   }
 }
 </script>
