bad3494b602df3d9fab681162f06977a96e73e71
Kevin Tsai <b2778173@hotmail.com>
2019-03-10 15:17:07 +0800

    Create sideBar.vue

---
+++ frontend/src/components/sideBar.vue
@@ -0,0 +1,107 @@
+<template>
+ <div>
+     <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              <span data-feather="home"></span>
+              Dashboard <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file"></span>
+              Orders
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+               <shopping-cart-icon class="custom-class"></shopping-cart-icon>
+              Products
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="users"></span>
+              Customers
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="bar-chart-2"></span>
+              Reports
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="layers"></span>
+              Integrations
+            </a>
+          </li>
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Saved reports</span>
+          <a class="d-flex align-items-center text-muted" href="#">
+            <span data-feather="plus-circle"></span>
+          </a>
+        </h6>
+        <ul class="nav flex-column mb-2">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Current month
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Last quarter
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <file-text-icon class="custom-class"></file-text-icon>
+              Social engagement
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Year-end sale
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+ </div>
+</template>
+<!-- .js 檔案在此相依 -->
+<script>
+import { FileTextIcon, ShoppingCartIcon  } from 'vue-feather-icons'
+
+export default {
+    name: '',
+    mixins: [],
+    extends: {},
+    components: { FileTextIcon, ShoppingCartIcon  },
+    props: [],
+    data() {
+        return {
+        };
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    updated() {},
+    beforeDestroy() {},
+    methods: {},
+};
+</script>
+
+<style>
+    /* @import url(../../static/assets/css/sideBar.css); */
+    @import url(../../static/assets/css/dashBoard.css);
+</style>
